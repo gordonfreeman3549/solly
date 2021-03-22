ebfca818fb012833f035829a331c12271685d818
gordonfreeman3549 <75548167+gordonfreeman3549@users.noreply.github.com>
2021-03-22 11:52:54 +0100

    stuff
    removed music stuff, added 2 rng commands

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 import os
-import youtube_dl
+import random
 from keep_alive import keep_alive
 from discord.ext import commands
 
@@ -9,7 +9,9 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = ']')
 
+
 players = {}
+
 #this is a latency command?
 @client.command()
 async def ping(ctx):
@@ -18,27 +20,124 @@ async def ping(ctx):
 @client.command()
 async def e(ctx):
   await ctx.send('fuck off this is not TF2')
+
+@client.command()
+async def ver(ctx):
+  await ctx.send('VOX InDev Version 0.1.2b')
+
+@client.command()
+async def mvm(ctx):
+  await ctx.send('HERE THEY COME! 5, 4, 3, 2, 1 *mvm music*')
+
+@client.command()
+async def test(ctx):
+  await ctx.send('Nuh huh, your mom')
+
+@client.command()
+async def everyone(ctx):
+  await ctx.send('`@`evryune!!!')
+
+@client.command()
+async def r34(ctx):
+  await ctx.send('How about no, you horny fuck.')
+
+@client.command()
+async def funny(ctx):
+  await ctx.send('bababooey')
+
+@client.command()
+async def lines(ctx):
+  await ctx.send("Currently `155` lines of code has been written")
+ 
+@client.command(pass_context=True)
+async def hldm(ctx):
+  plr = ["Gordon Freeman killed",
+         "Scout killed",
+         "Soldier killed",
+         "Pyro killed",
+         "Demoman killed",
+         "Engineer killed",
+         "Heavy killed",
+         "Medic killed",
+         "Sniper killed",
+         "Spy killed",
+         "Jesus killed",
+         "Agent Jones killed"]
   
+  die = ["Gordon Freeman with",
+         "Scout with",
+         "Soldier with",
+         "Pyro with",
+         "Demoman with",
+         "Engineer with",
+         "Heavy with",
+         "Medic with",
+         "Sniper with",
+         "Spy with",
+         "Jesus with",
+         "Agent Jones with"]
 
-#ok so the music doesn't work in repl.it so umm i will do this locally
+  deathreason = ["Crowbar",
+                 "Glock 17",
+                 ".357 Magnum",
+                 "MP5",
+                 "SPAS-12",
+                 "Crossbow",
+                 "Tau Cannon",
+                 "Gauss Cannon",
+                 "RPG",
+                 "Fragmentation Grenade",
+                 "Trimpmine",
+                 "Satchel Charge",
+                 "Fall Damage",
+                 "Physics Gun",
+                 "Item",
+                 "M1 Garand",
+                 "AK-47",
+                 "The hand of God",
+                 "P90",
+                 "Minigun",
+                 "C4",
+                 "USP Match",
+                 "MP7",
+                 "AR2",
+                 "Scattergun",
+                 "Pistol",
+                 "Double Barrel Shotgun",
+                 "Rocket Launcher",
+                 "Bat",
+                 "Shovel",
+                 "Flamethrower",
+                 "Fireaxe",
+                 "Grenade Launcher",
+                 "Stickybomb Launcher",
+                 "Bottle",
+                 "Fist",
+                 "Wrench",
+                 "Sentry LVL 1",
+                 "Sentry LVL 2",
+                 "Sentry Bullet LVL 3",
+                 "Sentry Rocket LVL 3",
+                 "Nailgun",
+                 "Bonesaw",
+                 "Sniper Rifle",
+                 "UZI",
+                 "Sword",
+                 "Sapper",
+                 "Butterfly Knife",
+                 "Butterfly Knife (backstab"]
+  await ctx.send((random.choice(plr), random.choice(die), random.choice(deathreason)))
+
 @client.command(pass_context=True)
-async def join(ctx):
-  channel = ctx.message.author.voice.voice_channel
-  await client.join_voice_channel(channel)
-
-@client.command(pass_context = True)
-async def leave(ctx):
-  server = ctx.message.server
-  voice_client = client.voice_client_in(server)
-  await voice_client.disconnect()
-
-@client.command(pass_context=True)
-async def play(ctx, url):
-  server = ctx.message.server
-  voice_client = client.voice_client_in(server)
-  player = await voice_client.create_ytdl_player(url)
-  players[server.id] = player
-  player.start()
+async def yomama(ctx):
+  yomjoke = ["Yer mum is so Bri'ish, she went to hell",
+             "Yo mama so lesbian, she couldn't give birth to a guy",
+             "Yo mama so small, she rode Pickle Rick's pickle'd dick",
+             "Yo mama so fat, that she was a TF2 map (a good map btw)",
+             "Your mother is so morbidly obese, that she has high blood pressure and type 2 diabetes",
+             "Yo mama so fat, she out chungus'd, BIG CHUNGUS!",
+             "Yo mama so stupid, she couldn't think of a yo mama joke"]
+  await ctx.send((random.choice(yomjoke)))
 
 
 #this part is fixed now it shows an epic status i guess?
